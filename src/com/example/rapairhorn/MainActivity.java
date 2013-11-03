b1344bfc0f0f688c5cbb716206a882400a3593cc
Sam Saccone <samccone@gmail.com>
2013-11-03 18:37:36 -0500

    inital

---
+++ src/com/example/rapairhorn/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.rapairhorn;
+
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Button btn1 = (Button)findViewById(R.id.button1);	
+		
+		btn1.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				final MediaPlayer mp1 = MediaPlayer.create(getBaseContext(), R.raw.rap_air_horn);  
+		        mp1.start();
+			}
+		});
+	}
+	
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
